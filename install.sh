52814978a51da2d83772838df4c7e55daf5f3969
jimzhan <jim.zhan@me.com>
2014-04-27 22:42:23 +0800

    segregate existing/empty dotfiles

--- install.sh
+++ install.sh
@@ -30,37 +30,37 @@ link() {
 echo "================================================================================"
 printf '\033[0;34m%s\033[0m\n' "Setting up preferences..."
 
-if [ ! -e $prefs/.git ]; then
-  echo "Start cloning..."
+
+if [ -d $prefs ]; then
+  #TODO update
+elif
+  # setup a brand new system
   git clone --recursive $repos $prefs
-else
-  echo "Start updating..."
+  cd $prefs
+  # fetch submodules (zsh)
+  git submodule foreach git pull
+
+  # zsh (as dotfiles's submodule)
+  printf '\033[0;34m%s\033[0m\n' "Setting up ZSH..."
+  export ZSH="$zsh"
+  chsh -s /bin/zsh
+  link $prefs/zshrc	$HOME/.zshrc
+  printf '\033[0;34m%s\033[0m\n' "Switching into new ZSH environment..."
+  source $HOME/.zshrc
+
+  # vim
+  printf '\033[0;34m%s\033[0m\n' "Setting up editors..."
+  #curl http://j.mp/spf13-vim3 -L -o - | sh
+  #lnif $prefs/vimrc.local $HOME/.vimrc.local
+  #lnif $prefs/gvimrc.local $HOME/.gvimrc.local
+  link $prefs/emacs    $HOME/.emacs
+  link $prefs/emacs.d  $HOME/.emacs.d
+
+  # dev
+  printf '\033[0;34m%s\033[0m\n' "Setting up dev. tools..."
+  link $prefs/hg               $HOME/.hg
+  link $prefs/hgrc             $HOME/.hgrc
+  link $prefs/gitconfig        $HOME/.gitconfig
+  link $prefs/gitignore_global $HOME/.gitignore_global
 fi
 
-cd $prefs
-# fetch submodules (zsh)
-git submodule foreach git pull
-
-
-# zsh (as dotfiles's submodule)
-printf '\033[0;34m%s\033[0m\n' "Setting up ZSH..."
-export ZSH="$zsh"
-chsh -s /bin/zsh
-link $prefs/zshrc	$HOME/.zshrc
-printf '\033[0;34m%s\033[0m\n' "Switching into new ZSH environment..."
-source $HOME/.zshrc
-
-# vim
-printf '\033[0;34m%s\033[0m\n' "Setting up editors..."
-#curl http://j.mp/spf13-vim3 -L -o - | sh
-#lnif $prefs/vimrc.local $HOME/.vimrc.local
-#lnif $prefs/gvimrc.local $HOME/.gvimrc.local
-link $prefs/emacs    $HOME/.emacs
-link $prefs/emacs.d  $HOME/.emacs.d
-
-# dev
-printf '\033[0;34m%s\033[0m\n' "Setting up dev. tools..."
-link $prefs/hg               $HOME/.hg
-link $prefs/hgrc             $HOME/.hgrc
-link $prefs/gitconfig        $HOME/.gitconfig
-link $prefs/gitignore_global $HOME/.gitignore_global
