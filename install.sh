4fc4a6047cc804f363ddaafd2a13ee31ee69f483
jimzhan <jim.zhan@me.com>
2014-04-27 22:36:55 +0800

    add source update post ZSH installation

--- install.sh
+++ install.sh
@@ -17,12 +17,6 @@ die() {
   exit 1
 }
 
-lnif() {
-  if [ ! -e $2 ] ; then
-    ln -s $1 $2
-  fi
-}
-
 # force remove any existing link or file before creating custom links.
 link() {
   if [ -h $2 ] ; then
@@ -53,18 +47,20 @@ printf '\033[0;34m%s\033[0m\n' "Setting up ZSH..."
 export ZSH="$zsh"
 chsh -s /bin/zsh
 link $prefs/zshrc	$HOME/.zshrc
+printf '\033[0;34m%s\033[0m\n' "Switching into new ZSH environment..."
+source $HOME/.zshrc
 
 # vim
-printf '\033[0;34m%s\033[0m\n' "Setting up VIM..."
+printf '\033[0;34m%s\033[0m\n' "Setting up editors..."
 #curl http://j.mp/spf13-vim3 -L -o - | sh
 #lnif $prefs/vimrc.local $HOME/.vimrc.local
 #lnif $prefs/gvimrc.local $HOME/.gvimrc.local
-#link $prefs/emacs    $HOME/.emacs
-#link $prefs/emacs.d  $HOME/.emacs.d
+link $prefs/emacs    $HOME/.emacs
+link $prefs/emacs.d  $HOME/.emacs.d
 
 # dev
 printf '\033[0;34m%s\033[0m\n' "Setting up dev. tools..."
-#link $prefs/hg               $HOME/.hg
-#link $prefs/hgrc             $HOME/.hgrc
+link $prefs/hg               $HOME/.hg
+link $prefs/hgrc             $HOME/.hgrc
 link $prefs/gitconfig        $HOME/.gitconfig
 link $prefs/gitignore_global $HOME/.gitignore_global
