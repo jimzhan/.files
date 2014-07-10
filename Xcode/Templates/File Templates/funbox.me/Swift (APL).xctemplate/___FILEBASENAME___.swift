65f098bcc7e0b735edb26ee3174090869c449db3
jimzhan <jim.zhan@me.com>
2014-07-10 08:20:52 +0800

    update iOS file template's declarations

--- Xcode/Templates/File Templates/funbox.me/Swift (APL).xctemplate/___FILEBASENAME___.swift
+++ Xcode/Templates/File Templates/funbox.me/Swift (APL).xctemplate/___FILEBASENAME___.swift
@@ -1,11 +1,12 @@
 /**
+ *  ------------------------------------------------------------
  *  @project ___PROJECTNAME___
  *  @file    ___FILENAME___
  *  @author  ___FULLUSERNAME___
  *
  *
  *  Copyright © ___YEAR___ ___FULLUSERNAME___.
- *
+ *  ------------------------------------------------------------
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
@@ -17,6 +18,10 @@
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
+ *  ------------------------------------------------------------
  */
 
 
+class ___FILEBASENAME___ {
+
+}
--- Xcode/Templates/Project Templates/iOS/Application/CocoaPods Application.xctemplate/Bridging-Header.h
+++ Xcode/Templates/Project Templates/iOS/Application/CocoaPods Application.xctemplate/Bridging-Header.h
@@ -1,4 +1,5 @@
 /**
+ *  ------------------------------------------------------------
  *  @project ___PROJECTNAME___
  *  @file    ___FILENAME___
  *  @author  ___FULLUSERNAME___
@@ -8,7 +9,7 @@
  *  headers that you would like to expose to Swift.
  *
  *  Copyright © ___YEAR___ ___FULLUSERNAME___.
- *
+ *  ------------------------------------------------------------
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
@@ -20,6 +21,7 @@
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
+ *  ------------------------------------------------------------
  */
 
 
