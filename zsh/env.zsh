64f112d3d7e09aa072b172fb2fd4f39ae327ea99
jimzhan <jim.zhan@me.com>
2014-11-13 09:56:02 +0800

    structual updates for zsh settings

---
+++ zsh/env.zsh
@@ -0,0 +1,24 @@
+#-------------------
+# LS Colors
+#-------------------
+if [ -x /usr/local/bin/gdircolors ]; then
+  eval $(gdircolors $HOME/.dotfiles/dircolors/dircolors.256dark)
+fi
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+
+export EDITOR="vim"
+#export GOPATH="$HOME/Go"
+export GOVERSION=$(brew list go | head -n 1 | cut -d '/' -f 6)
+export Go=$(brew --prefix)/Cellar/go/$GOVERSION
+export PYTHONPATH=".:$HOME/Projects/tornext:$PYTHONPATH"
+export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$HOME/bin:$HOME/local/bin:$Go/bin:$Go/libexec/bin:$PATH"
+export MANPATH="/usr/local/man:$MANPATH"
+
+# You may need to manually set your language environment
+# export LANG=en_US.UTF-8
+
+# Compilation flags
+# export ARCHFLAGS="-arch x86_64"
+
+# ssh
+# export SSH_KEY_PATH="~/.ssh/dsa_id"
