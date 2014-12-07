6397e24d39885d1779c7fdd9255195ad2549f5a5
jimzhan <jim.zhan@me.com>
2014-12-07 17:40:21 +0800

    remove emacs & bash

--- emacs/emacs.d/.emacs.d/fonts.el
+++
@@ -1,3 +0,0 @@
-(set-fontset-font (frame-parameter nil 'font) 'unicode '("STHeiti" . "unicode-bmp") 'nil 'append)
-(set-fontset-font (frame-parameter nil 'font) 'unicode '("LiHei Pro" . "unicode-bmp") 'nil 'append)
-(set-fontset-font (frame-parameter nil 'font) 'unicode '("Arial Unicode MS" . "unicode-bmp") 'nil 'append)
